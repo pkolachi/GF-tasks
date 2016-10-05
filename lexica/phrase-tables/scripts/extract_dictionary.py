7818069ca875ddb99763a874848c15cf0bc369ef
Prasanth Kolachina <prasanth.kolachina@cse.gu.se>
2016-10-05 20:51:31 +0200

    first version of convex solution

---
+++ lexica/phrase-tables/scripts/extract_dictionary.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+# This is function to check KL-divergence between
+# a conditional distribution and its inverse
+
+from __future__ import division, print_function;
+import numpy as np;
+import random;
+import cvxpy as cvx;
+
+
+def test():
+  from collections import Counter;
+  from itertools import product;
+  from math import log;
+  from operator import itemgetter;
+  import string;
+
+  def simulate_KL(space=1000, tries=20):
+    alnum = string.ascii_letters + string.digits;
+    printable = string.printable;
+    population = list(product(alnum, alnum, printable));
+    kldivergence = lambda X, Y: sum(\
+        x*log(x)-x*log(y) for x,y in zip(X, Y) );
+
+    for trial in range(tries):
+      random.shuffle(population);
+      samples    = random.sample(population, space);
+      trigrams   = Counter(samples);
+      forwardmarginals   = Counter(map(itemgetter(0,1), samples));
+      backwardmarginals  = Counter(map(itemgetter(2), samples));
+      forwardconddist    = [trigrams[word]/forwardmarginals[word[:-1]] \
+          for word in sorted(trigrams.keys())];
+      backwardconddist   = [trigrams[word]/backwardmarginals[word[-1]] \
+          for word in sorted(trigrams.keys())];
+
+      dist = kldivergence(forwardconddist, backwardconddist);
+      print(dist, dist/space);
+
+  for space in (10000, 100000, 10000):
+    simulate_KL(space=space);
+  return;
+
+def convex_cleanup(phrase_probs, lex_probs):
+  size = phrase_probs.size;
+  try:
+    assert(size == lex_probs.size);
+  except AssertionError:
+    print("The probability distribution does not have the same size", file=stderr);
+    return;
+
+  sparse_dist = cvx.Variable(size);
+  lambd = cvx.Parameter(sign="positive");
+  reg   = cvx.norm(sparse_dist, 1);
+
+  phrase_cost = cvx.sum_entries(cvx.kl_div(sparse_dist, phrase_probs));
+  lex_cost    = cvx.sum_entries(cvx.kl_div(sparse_dist, lex_probs));
+  cost = phrase_cost+lex_cost+lambd*reg;
+
+  for reg_param in np.arange(0, 1, 0.01):
+    lambd.value = reg_param;
+    opt_instance = cvx.Problem(cvx.Minimize(cost), \
+        [sparse_dist >= 0, # Positive probability values \
+        ]);
+    opt_instance.solve();
+    yield sparse_dist;
+
+def tester():
+  N = 10000;
+  pprobs  = np.random.sample(N);
+  lprobs  = np.random.sample(N);
+  solutions = convex_cleanup(pprobs, lprobs);
+  for sol in solutions:
+    new_dist = sol.value;
+    print(np.count_nonzero(new_dist), np.min(new_dist), np.max(new_dist));
+  return;
+
+tester();
+
